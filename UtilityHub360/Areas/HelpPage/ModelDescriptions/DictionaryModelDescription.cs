bb352c76fce0f71d2a4d666b34bfd5b6aa2904e7
elie <elibarabarquez.dev@mail.com>
2025-09-16 20:25:24 +0300

    add files

---
+++ UtilityHub360/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UtilityHub360.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ UtilityHub360/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UtilityHub360.WebApiApplication" Language="C#" %>
