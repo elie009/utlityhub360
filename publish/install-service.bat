e7b01046f8e243a72b2648885ab7f690fb66aa4a
elie <elibarabarquez.dev@mail.com>
2025-11-15 19:33:04 +0300

    Remove appsettings files and publish folders with secrets from git tracking

--- publish/install-service.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing UtilityHub360 as Windows Service...
-
-sc create "UtilityHub360" binPath="C:\path\to\your\publish\folder\UtilityHub360.exe" start=auto
-sc description "UtilityHub360" "UtilityHub360 API Service"
-sc start "UtilityHub360"
-
-echo Service installed and started successfully!
-pause
--- publish/uninstall-service.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Uninstalling UtilityHub360 Windows Service...
-
-sc stop "UtilityHub360"
-sc delete "UtilityHub360"
-
-echo Service uninstalled successfully!
-pause
